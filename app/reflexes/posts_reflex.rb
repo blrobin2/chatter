ae931a7d3ceb1a61a7681723f1c6f4042266a3d4
Bennie Robinson <brobinson88@gmail.com>
2021-04-09 15:24:49 -0400

    broadcast likes/reposts over cable_ready

--- app/reflexes/posts_reflex.rb
+++ app/reflexes/posts_reflex.rb
@@ -4,10 +4,18 @@ class PostsReflex < ApplicationReflex
   def repost
     post = Post.find(element.dataset[:id])
     post.increment! :reposts_counter
+    cable_ready['timeline'].text_content(
+      selector: "#post-#{post.id}-reposts",
+      text: post.reload.reposts_counter
+    ).broadcast
   end
 
   def like
     post = Post.find(element.dataset[:id])
     post.increment! :likes_count
+    cable_ready['timeline'].text_content(
+      selector: "#post-#{post.id}-likes",
+      text: post.reload.likes_count
+    ).broadcast
   end
 end
